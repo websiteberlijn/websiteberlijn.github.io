377ec4b2760d34de1c9f9dead2e98940733c1baa
websiteberlijn <48692730+websiteberlijn@users.noreply.github.com>
2019-03-22 09:45:59 +0100

    Update berlijn-javascript.js

--- berlijn-javascript.js
+++ berlijn-javascript.js
@@ -1,26 +1,25 @@
-// Set the date we're counting down to
 var countDownDate = new Date("April 2 , 2019 22:30:00").getTime();
 
-// Update the count down every 1 second
+// Update de timer elke 1 seconde
 var x = setInterval(function() {
 
-  // Get todays date and time
+  // Pakt de datum van vandaag
   var now = new Date().getTime();
     
-  // Find the distance between now and the count down date
+  // zoekt het verschil tussen de 2 datums
   var distance = countDownDate - now;
     
-  // Time calculations for days, hours, minutes and seconds
+  // Berekent de dagen, uren, minuten en seconden
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
-  // Output the result in an element with id="demo"
+  // Zet de output in de webpagina met een id="demo"
   document.getElementById("demo").innerHTML = days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
     
-  // If the count down is over, write some text 
+  // Dit is zodat als de timer afgelopen is er een tekst komt te staan i.p.v. 0d 0h 0m 0s
   if (distance < 0) {
     clearInterval(x);
     document.getElementById("demo").innerHTML = "WE GAAN VERTREKKEN!";
